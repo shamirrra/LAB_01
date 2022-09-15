ea954bf60f0c2dec02250d59a44fd21ec4621445
Shamira Alifanindya Prasetya <shamira.alifanindya@ui.ac.id>
2022-09-15 14:32:30 +0700

    tutorial xml and json

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,11 +1,34 @@
+# Lab 1
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
 
+# Lab 2
+from django.http import HttpResponse
+from django.core import serializers
+
+# Lab 1
 data_barang_wishlist = BarangWishlist.objects.all()
+
 context = {
     'list_barang': data_barang_wishlist,
     'nama': 'Shamira'}
     
-# Create your views here.
 def show_wishlist(request):
     return render(request, "wishlist.html", context)
+
+# Lab 2
+data = BarangWishlist.objects.all()
+
+def show_xml(request):
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request,id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
