b4f29a4eecf120977a3e9789024deed969ad67da
Shamira Alifanindya Prasetya <shamira.alifanindya@ui.ac.id>
2022-09-22 14:57:58 +0700

    lab 3: form, authentication, session, and cookie

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -8,12 +8,25 @@ from wishlist.views import show_json
 from wishlist.views import show_xml_by_id
 from wishlist.views import show_json_by_id
 
+# Lab 3
+from wishlist.views import register
+from wishlist.views import login_user
+from wishlist.views import logout_user
+
 app_name = 'wishlist'
 
 urlpatterns = [
+    # Lab 1
     path('', show_wishlist, name='show_wishlist'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
+
+    # Lab 2
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<int:id>', show_json_by_id, name='show_json_by_id')
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
+
+    # Lab 3
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
     ]
